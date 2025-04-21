f18fa4bb732639213350adecf1720f7ef4a89d77
Malik Ashar <ashar1122@gmail.com>
2025-04-21 16:29:17 +0500

    update routes commit

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('home.html')  # Renders home.html file from templates folder
+
 
 if __name__ == '__main__':
     app.run(debug=True)
